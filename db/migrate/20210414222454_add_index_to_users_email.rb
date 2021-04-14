1f1bfbc7782145629c265e7a09618185ccd79575
takaya787 <takaya318318@gmail.com>
2021-04-15 07:47:55 +0900

    secureなpasswordを設定

--- db/migrate/20210414222454_add_index_to_users_email.rb
+++ db/migrate/20210414222454_add_index_to_users_email.rb
@@ -1,4 +1,6 @@
 class AddIndexToUsersEmail < ActiveRecord::Migration[6.1]
+  #データベースにインデックスを追加することで検索効率が向上する。また、データベースレベルでの一意性を保証するためにも使われる
+
   def change
     add_index :users, :email, unique: true
   end
