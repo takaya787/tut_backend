686d4a12ea272fba7ef8056cee7d38f0a4a063c5
takaya787 <takaya318318@gmail.com>
2021-04-22 15:10:41 +0900

    testの仕方　分からんので後で

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
 
+  # test "user show response test" do
+
+  #   assert_response :success
+  # end
 end
