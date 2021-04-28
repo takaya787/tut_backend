d3e553130133697bdc0def9d078d65a99cec5c4f
takaya787 <takaya318318@gmail.com>
2021-04-28 18:51:26 +0900

    preview mail完了

--- app/controllers/api/account_activations_controller.rb
+++ app/controllers/api/account_activations_controller.rb
@@ -1,6 +1,8 @@
 module Api
   class AccountActivationsController < ApplicationController
+    #userを有効化する時にclickされるaction
     def edit
+      render json: {message: 'Your account is activated!'}
     end
   end
 end
