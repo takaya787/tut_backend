c119b30b64fa091e69e7b5adaca5c4e1610fc28d
takaya787 <takaya318318@gmail.com>
2021-04-30 15:35:23 +0900

    Linkをクリックして ユーザー有効化　完了

--- app/controllers/api/account_activations_controller.rb
+++ app/controllers/api/account_activations_controller.rb
@@ -3,14 +3,14 @@ module Api
     #userを有効化する時にclickされるaction
     def edit
       email = CGI.unescape(params[:email])
-      user = User.find_by(email: email)
+      @user = User.find_by(email: email)
       # render json: {message: 'Your account is activated!',user:user}, status: :ok
-      if user && !user.activated? && user.authenticated?(:activation, params[:id])
-        user.update_attribute(:activated,    true)
-        user.update_attribute(:activated_at, Time.zone.now)
-        render json: {message: 'Your account is activated!',user:user,token: params[:id]}, status: :ok
-      elsif user.activated?
-        render json: {message: 'Your account is already activated!',user:user}, status: :ok
+      if @user && @user.authenticated?(:activation, params[:id])
+        @user.update_attribute(:activated,    true)
+        @user.update_attribute(:activated_at, Time.zone.now)
+        render 'users/activation.html.erb', status: :ok
+      # elsif @user.activated?
+      #   render json: {message: 'Your account is already activated!',user:@user}, status: :ok
       else
         render json: {message: 'Your account failed to be activated'}, status: :unprocessable_entity
       end
