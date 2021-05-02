a9f77d86b9c5b3722fd129c50804ad7ef9a82856
takaya787 <takaya318318@gmail.com>
2021-05-02 14:52:43 +0900

    password reset 関連　一旦終了

--- app/controllers/api/password_resets_controller.rb
+++ app/controllers/api/password_resets_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class PasswordResetsController < ApplicationController
+    before_action :get_user, only:[:edit,:update]
+    before_action :valid_user, only:[:edit,:update]
 
     #Post /password_reset
     def create
@@ -17,5 +19,32 @@ module Api
     def edit
       render "users/password_reset.html.erb",status: :ok
     end
+
+    #Put /password_reset/:id
+    def update
+      if @user.update(user_params)
+        render "users/password_reset.html.erb"
+      end
+    end
+
+    private
+      def user_params
+        params.require(:user).permit(:password, :password_confirmation)
+      end
+
+      def get_user
+        email = CGI.unescape(params[:email])
+        @user = User.find_by(email: email)
+      end
+
+      # 正しいユーザーかどうか確認する
+      def valid_user
+        # unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+        #   render json: {message:"You are not correct user"}, status: :bad_request
+        # end
+        unless @user && @user.activated? && @user.authenticated?(:reset, params[:id])
+          render json: {message:"You are not correct user"}, status: :bad_request
+        end
+      end
   end
 end
