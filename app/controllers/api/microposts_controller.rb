63e9d7dcc9fa467a271afc1cbc9b6d313703aec2
takaya787 <takaya318318@gmail.com>
2021-05-10 10:27:27 +0900

    micropost 作成、削除　完了

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -1,6 +1,36 @@
 module Api
   class MicropostsController < ApplicationController
-    def show
+    before_action :authorized, only: [:create, :destroy]
+    before_action :activated_current_user, only:[:create,:destroy]
+    before_action :set_variable, only: [:destroy]
+
+    def create
+      micropost = @current_user.microposts.build(micropost_params)
+      if micropost.save
+        render json: { micropost: micropost, message: 'Micropost created'},status: :ok
+      else
+        render json: { micropost: micropost, message: 'Fail to create Micropost'},status: :bad_request
+      end
     end
+
+    def destroy
+      if @current_user == @user || @current_user.admin?
+        @micropost.destroy
+        render json: {message: 'Micropost deleted'},status: :accepted
+      else
+        render json: {message: 'You are not correct user'},status: :unprocessable_entity
+      end
+    end
+
+    private
+      def micropost_params
+        params.require(:micropost).permit(:content)
+      end
+
+      def set_variable
+        @micropost = Micropost.find(params[:id])
+        @user = User.find(@micropost.user_id)
+      end
+
   end
 end
