2e9e36c33c750a2b873fbad19659dec0d2e7e5d7
takaya787 <takaya318318@gmail.com>
2021-05-19 14:15:35 +0900

    active-relationship done

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :microposts,dependent: :destroy
   has_many :active_relationships, class_name:"Relationship",foreign_key:"follower_id",dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   attr_accessor :activation_token,:reset_token
   before_save{ self.email.downcase!}
@@ -53,6 +54,21 @@ class User < ApplicationRecord
   # パスワード再設定のメールを送信する
   def send_password_reset_email
     UserMailer.password_reset(self).deliver_now
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following.push(other_user)
+  end
+
+  #ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしてたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
