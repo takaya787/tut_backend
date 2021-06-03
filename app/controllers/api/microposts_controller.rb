9a1753dfa076f515a9123f06cfc92cb2b20bb737
takaya787 <takaya318318@gmail.com>
2021-06-03 15:52:10 +0900

    micropost/showの二重render解消

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -6,9 +6,7 @@ module Api
     before_action :correct_user, only: [:destroy,:update]
     def show
       @gravator_url = gravator_for(@user)
-
-      render 'users/auto_relationships', formats: :json, handlers: 'jbuilder'
-      render 'microposts/show.json.jbuilder', status: :ok
+      render 'microposts/show', formats: :json, handlers: 'jbuilder'
     end
 
     def create
